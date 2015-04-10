326200acb88ef6013755a726a95f75be6f187654
Coos Baakman <cbaakman@gmail.com>
2015-04-10 15:04:59 +0200

    fixed disabled backend problem

--- hommod_rest/frontend/api/endpoints.py
+++ hommod_rest/frontend/api/endpoints.py
@@ -1,19 +1,19 @@
 import logging
 
 from flask import Blueprint, jsonify, request
-from hommod_rest.services.utils import (
-    extract_info, extract_alignment, extract_model)
+
+from hommod_rest.managejobs import create_model
+from hommod_rest.services.utils import (extract_info, extract_alignment,
+                                        extract_model)
 
 _log = logging.getLogger(__name__)
 
-from hommod_rest.managejobs import add_create_model_job
 
 bp = Blueprint('hommod', __name__, url_prefix='/api')
 
 
 @bp.route('/submit/', methods=['POST'])
 def submit():
-
     sequence = request.form.get('sequence', None)
     position = request.form.get('position', None)
     species_id = request.form.get('species_id', None)
@@ -28,7 +28,7 @@ def submit():
     _log.debug("submitted ( sequence: %s, species: %s, position: %i )"
                % (sequence, species_id, position))
 
-    jobid = add_create_model_job(sequence, species_id, position)
+    jobid = create_model(sequence, species_id, position)
     print jobid
 
     return jsonify({'jobid': jobid})
@@ -36,18 +36,15 @@ def submit():
 
 @bp.route('/status/<jobid>/', methods=['GET'])
 def status(jobid):
-
-    from hommod_rest.tasks import create_model
-    result = create_model.AsyncResult(jobid)
-    print jobid
+    from hommod_rest.application import celery
+    result = celery.AsyncResult(jobid)
     return jsonify({'status': result.status})
 
 
 @bp.route('/results/<jobid>/', methods=['GET'])
 def results(jobid):
-
-    from hommod_rest.tasks import create_model
-    result = create_model.AsyncResult(jobid)
+    from hommod_rest.application import celery
+    result = celery.AsyncResult(jobid)
 
     if result.ready():
 
