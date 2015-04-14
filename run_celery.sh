0fbc3cc3bf8a00690cb2bb932f5820b1c04b9e58
Coos Baakman <cbaakman@gmail.com>
2015-04-14 13:37:10 +0200

    log to stdout

--- run_celery.sh
+++ run_celery.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 export HOMMOD_REST_SETTINGS='../dev_settings.py'
-celery -A hommod_rest.application:celery worker -B --logfile=celery.log
+celery -A hommod_rest.application:celery worker -B
