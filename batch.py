5e8581c3db6ba996e157b622ee5c4217f7804a1a
cbaakman <c.baakman@umcn.nl>
2015-09-07 10:05:23 +0200

    comments added

--- batch.py
+++ batch.py
@@ -1,9 +1,14 @@
+#!/usr/bin/python
+
+# This is a script for building models without using celery or the restful service.
+# It can be run from console and uses the same modeling procedure as the restful service does.
+
 import logging
 import sys
 import os
 import traceback
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append (os.path.dirname (os.path.abspath (__file__)))
 from hommod_rest.services.model import modeler
 from hommod_rest.services.modelutils import parseFasta
 import hommod_rest.default_settings as config
@@ -13,7 +18,8 @@ _log = logging.getLogger(__name__)
 
 def init():
 
-    # Initialise services
+    # Initialise services:
+
     from hommod_rest.services.model import modeler
     modeler.yasara_dir = config.YASARADIR
     modeler.execution_root_dir = config.EXECUTIONDIR
@@ -37,19 +43,30 @@ def init():
     aligner.msaExe = config.MSA
 
 
-if len(sys.argv) != 3 or not os.path.isfile(sys.argv[2]):
+if len (sys.argv) != 3 or not os.path.isfile (sys.argv[2]):
     print 'usage: %s [species id] [fasta filepath]' % sys.argv[0]
     sys.exit(1)
 
 init()
 
-species = sys.argv[1]
-seqs = parseFasta(open(sys.argv[2],'r'))
+# The script takes a uniprot species ID and fasta filepath as input.
+# Fasta headers are ignored, all sequences are assumed to be
+# from the same species.
+#
+# A model is attempted to be built for every sequence in the fasta.
 
-for ID in seqs.keys():
+species = sys.argv [1]
+seqs = parseFasta (open (sys.argv [2],'r'))
+
+for ID in seqs:
+
     try:
-        modeler.modelProc(seqs[ID], species)
+
+        modeler.modelProc (seqs[ID], species)
+
     except:
+        # Print the error to stdout, but don't quit.
+
         exc_type, exc_value, exc_traceback = sys.exc_info()
-        stacktrace = ''.join(traceback.format_exception(exc_type, exc_value, exc_traceback))
+        stacktrace = ''.join(traceback.format_exception (exc_type, exc_value, exc_traceback))
         print stacktrace
