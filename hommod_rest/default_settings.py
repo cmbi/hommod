73bde041845f37376e03813ba98b167e1f950dff
cbaakman <c.baakman@umcn.nl>
2015-09-07 10:18:36 +0200

    comments on configuration

--- hommod_rest/default_settings.py
+++ hommod_rest/default_settings.py
@@ -1,5 +1,6 @@
 from kombu import Exchange, Queue
 
+
 # Celery
 CELERY_ACCEPT_CONTENT = ['pickle', 'json', 'msgpack', 'yaml']
 CELERY_BROKER_URL = 'amqp://guest@localhost'
@@ -11,7 +12,10 @@ CELERY_QUEUES = (
 CELERY_TRACK_STARTED = True
 CELERY_RESULT_BACKEND = 'redis://localhost/1'
 
+
+# Time it takes for a model to get outdated:
 MAX_MODEL_DAYS = 100
+
 
 # Email logging settings
 MAIL_SERVER = "131.174.165.22"
@@ -19,17 +23,28 @@ MAIL_SMTP_PORT = 25
 MAIL_FROM = "hommod-rest@cmbi.umcn.nl"
 MAIL_TO = ["Coos.Baakman@radboudumc.nl"]
 
-# Paths
 
+# Directory where dssp files are stored:
 DSSPDIR = '/mnt/cmbi4/dssp/'
+
+# Output directory to store models in:
 MODELDIR = '/data/models/'
+
+# Working directory for yasara:
 EXECUTIONDIR = '/data/tmp/'
+
+# Directory where interpro files are stored:
 INTERPRODIR = '/data/interpro/'
 
-MSA = '/data/prog/pairwiseUBUNTU/MSA'
-BLASTP = '/usr/bin/blastp'
+# Executables:
+MSA = '/data/prog/pairwiseUBUNTU/MSA' # made by Joanna Lange
+BLASTP = '/usr/bin/blastp' # ncbi
 CLUSTAL = '/usr/bin/clustalw'
 INTERPROSCAN = '/data/prog/interproscan-5.8-49.0/interproscan.sh'
+
+# Yasara installation:
 YASARADIR = '/data/prog/Yasara/yasara/'
+
+# Blast databases:
 SPECIESDBDIR = '/data/blast/uniprot-species/'
 TEMPLATESDB = '/data/blast/templates/templates'
