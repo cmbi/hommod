89fad94d1a960efb63fcd72029cabc8ab7664df6
cbaakman <cbaakman@gmail.com>
2016-04-22 10:51:58 +0200

    sleep between polls

--- tests/model_request.py
+++ tests/model_request.py
@@ -1,4 +1,5 @@
 import requests
+from time import sleep
 
 payload = {
     'position': 47, 'species_id': 'HUMAN',
@@ -13,14 +14,24 @@ response = requests.post('http://localhost:7001/api/submit/', data=payload)
 
 jobid = response.json () ['jobid']
 
+prevstatus = ''
 while True:
 
     response = requests.get ('http://localhost:7001/api/status/%s/' % jobid)
     status = response.json () ['status']
 
-    print status
+    if status != prevstatus:
+        print status
+
+    prevstatus = status
+
     if status not in ["PENDING", "STARTED", "RUNNING"]:
         break
 
+    sleep (10)
+
 if status != "SUCCESS":
     raise Exception (response.message)
+
+response = requests.get ('http://localhost:7001/api/get_model_file/%s.pdb' % jobid)
+print response.text
