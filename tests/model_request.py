abd144d6cfaf958574c307fd9205a9e8489d8d11
cbaakman <cbaakman@gmail.com>
2016-04-22 10:29:49 +0200

    test now prints the final result

--- tests/model_request.py
+++ tests/model_request.py
@@ -1,14 +1,26 @@
 import requests
 
 payload = {
-    'position': 12, 'species_id': 'RAT',
+    'position': 47, 'species_id': 'HUMAN',
     'sequence': """
-MLATRALSLIGKRAISTSVCLRAHGSVVKSEDYALPSYVDRRDYPLPDVAHVKLLSASQK
-ALKEKEKADWSSLSRDEKVQLYRIQFNESFAEMNKGTNEWKTVVGLAMFFIGFTALVLIW
-EKSYVYGPIPHTFDRDWVAMQTKRMLDMKVNPIQGFSAKWDYNKNEWKK
+VHLTDAEKAAVSCLWGKVNSDEVGGEALGRLLVVYPWTQRYFDSFGDLSSASAIGNAKVK
+AHGKKVITAFNDGLNHLDSLKGTFASLSELHCDKLHVDPENFRLLGNIVIVLGHHLGKDF
+TPAAQAAFQKVVAGVATALAHKYH
 """.replace('\n', '')
 }
 
-response = requests.post('http://localhost:6000/api/submit/', data=payload)
+response = requests.post('http://localhost:7001/api/submit/', data=payload)
 
-print response.text
+jobid = response.json () ['jobid']
+
+while True:
+
+    response = requests.get ('http://localhost:7001/api/status/%s/' % jobid)
+    status = response.json () ['status']
+
+    print status
+    if status not in ["PENDING", "STARTED", "RUNNING"]:
+        break
+
+if status != "SUCCESS":
+    raise Exception (response.message)
