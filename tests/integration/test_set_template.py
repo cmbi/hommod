94f6937cf23f1ab851256b897eeb416a28c6fb9b
Coos Baakman <cbaakman@radboudumc.nl>
2016-11-03 16:43:50 +0100

    renamed to setup and teardown

--- tests/integration/test_set_template.py
+++ tests/integration/test_set_template.py
@@ -3,18 +3,18 @@ from hommod_rest.services.model import modeler
 import hommod_rest.default_settings as config
 
 
-def _setup():
+def setup():
     modeler.yasara_dir = config.YASARADIR
     modeler.execution_root_dir = config.EXECUTIONDIR
     modeler.model_root_dir = config.MODELDIR
     modeler.template_blacklist = config.TEMPLATE_BLACKLIST
 
 
-def _tear_down():
+def teardown():
     pass
 
 
-@with_setup(_setup, _tear_down)
+@with_setup(setup, teardown)
 def test_set_template():
     obj, oligomerization = modeler._set_template("3DS1")
 
