b7fbbdf50c973d1f16b1a2928634a3b79b53b7f5
Coos Baakman <cbaakman@radboudumc.nl>
2018-04-16 13:46:52 +0200

    test for method

--- tests/unit/controllers/test_domain.py
+++ tests/unit/controllers/test_domain.py
@@ -3,7 +3,7 @@ from nose.tools import eq_
 from hommod.controllers.domain import domain_aligner
 from hommod.models.range import SequenceRange
 from hommod.models.align import TargetTemplateAlignment
-
+from hommod.models.template import TemplateID
 
 
 def test_get_template_sequence_in_target_range():
@@ -31,3 +31,25 @@ def test_get_template_sequence_in_target_range():
 
     template_seq = domain_aligner._get_template_sequence_in_target_range(alignment, range_)
     eq_(template_seq, "ISLWKAESISLDFGDHQADLLNCKD-SIISNANVKEFWDGFEEVSKR-")
+
+
+def test_find_shared_hits_ranges():
+    d = {
+        SequenceRange(1, 22, "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"):
+            TargetTemplateAlignment("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+                                    "VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV"),
+        SequenceRange(3, 18, "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"):
+            TargetTemplateAlignment("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+                                    "VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV"),
+        SequenceRange(3, 19, "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"):
+            TargetTemplateAlignment("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+                                    "VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV")
+    }
+    template_id = TemplateID('1xxx', 'A')
+    for v in d.values():
+        v.template_id = template_id
+
+    r = domain_aligner._find_shared_hits_ranges(d)
+
+    eq_(len(r), 1)
+    eq_(len(r.values()[0]), 3)
---
+++ tests/unit/controllers/test_models.py
@@ -0,0 +1,44 @@
+import tempfile
+import os
+
+from nose.tools import eq_
+from mock import patch
+
+from hommod.controllers.model import modeler
+from hommod.controllers.storage import model_storage
+from hommod.models.align import TargetTemplateAlignment
+
+
+@patch("tarfile.open")
+def test_selected_targets_format(mock_open):
+
+    targets = {'a': 'coffee',
+               'b': 'tea'}
+
+    alignments = {}
+    for chain_id in targets:
+        alignments[chain_id] = TargetTemplateAlignment('', '')
+        alignments[chain_id].target_id = targets[chain_id]
+
+    path = tempfile.mktemp()
+
+    class FakeTarFile:
+        def __enter__(self, *args, **kwargs):
+            return self
+
+        def __exit__(self, *args, **kwargs):
+            pass
+
+        def extractfile(self, *args, **kwargs):
+            return open(path, 'r')
+
+    mock_open.return_value = FakeTarFile()
+
+    try:
+        modeler._write_selected_targets(alignments, path)
+        parsed = model_storage.extract_selected_targets('no.tar.gz')
+    finally:
+        if os.path.isfile(path):
+            os.remove(path)
+
+    eq_(parsed, targets)
