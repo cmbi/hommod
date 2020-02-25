e5511d9383b8081a21676396d47f74bcd72109a8
Coos Baakman <cbaakman@radboudumc.nl>
2020-02-25 11:04:59 +0100

    yasara can't work with 'U' in the input sequence

--- hommod/controllers/sequence.py
+++ hommod/controllers/sequence.py
@@ -7,9 +7,9 @@ def is_nucleic_acid_sequence(s):
     return all([is_nucleotide_char(s[i]) for i in range(len(s))])
 
 def is_amino_acid_char(c):
-    return c in ['A','B','C','D','E','F','G','H','I','J','K',
-                 'L','M','N','O','P','Q','R','S','T','U','V',
-                 'W','X','Y','Z']
+    return c in ['A','C','D','E','F','G','H','I','K',
+                 'L','M','N','P','Q','R','S','T','V',
+                 'W','Y']
 
 def is_nucleotide_char(c):
     return c in ['a', 't', 'c', 'u', 'g']
